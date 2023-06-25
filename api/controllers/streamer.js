7dc0f653ce636e2124a541005ecfb8b0f5020b4f
Lukass9 <l.pisarek@o2.pl>
2023-06-25 17:33:00 +0200

    added addstreamer and getStreamers controllers

--- api/controllers/streamer.js
+++ api/controllers/streamer.js
@@ -1,6 +1,20 @@
 import { db } from "../db.js";
 
-export const addStreamer = (req, res) => {};
+export const addStreamer = (req, res) => {
+  const q = "INSERT INTO Streamers(`id`, `name`, `striming_platform`, `desc`)";
+
+  const values = [
+    req.body.id,
+    req.body.name,
+    req.body.striming_platform,
+    req.body.desc,
+  ];
+
+  db.all(q, [values], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.json("Streamers has been added.");
+  });
+};
 export const getStreamers = (req, res) => {
   const q = "SELECT * FROM Streamers";
 
