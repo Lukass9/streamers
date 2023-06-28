c81031dc94b5a9d8672936c9abe2ed50b3abad6e
Lukass9 <l.pisarek@o2.pl>
2023-06-28 06:21:44 +0200

    create socket folder

--- socket/index.js
+++ socket/index.js
@@ -1,8 +1,9 @@
 import { createServer } from "http"; // Importuj funkcję createServer z modułu http
 import { Server } from "socket.io"; // Importuj klasę Server z modułu socket.io
-// Utwórz serwer HTTP za pomocą funkcji createServer
-const server = createServer(app);
+import sqlite3 from "sqlite3";
 
+// Utwórz serwer HTTP za pomocą funkcji createServer
+const server = createServer();
 // Utwórz instancję serwera Socket.io i przekaż serwer HTTP jako argument
 export const io = new Server(server, {
   cors: {
@@ -10,24 +11,59 @@ export const io = new Server(server, {
   },
 });
 
-// Obsługa połączenia WebSocket
-io.on("connection", (socket) => {
-  console.log("Nowe połączenie WebSocket");
+const db = new sqlite3.Database("../api/database.db");
 
-  socket.on("disconnect", () => {
-    console.log("Rozłączono WebSocket");
+function sendUpdatedData() {
+  db.all("SELECT * FROM Streamers", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      io.sockets.emit("dataUpdated", data);
+    }
   });
+}
 
-  socket.emit("streamers", emitStreamersUpdated());
-  // Obsługa zdarzeń i logika WebSocket
-  // ...
+// Nasłuchiwanie na połączenia klientów
+io.on("connection", (socket) => {
+  console.log("Nowy klient podłączony");
+
+  // Wysyłanie aktualnych danych połączonemu klientowi
+  db.all("SELECT * FROM Streamers", (err, rows) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    socket.emit("dataUpdated", rows);
+  });
 });
 
-export async function emitStreamersUpdated() {
-  try {
-    const streamers = await getStreamers();
-    io.emit("streamers", streamers);
-  } catch (error) {
-    console.log(error);
-  }
-}
+// Nasłuchiwanie na zmiany w bazie danych
+db.on("update", sendUpdatedData);
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Serwer Socket.IO nasłuchuje na porcie ${port}`);
+});
+
+// // Obsługa połączenia WebSocket
+// io.on("connection", (socket) => {
+//   console.log("Nowe połączenie WebSocket");
+
+//   socket.on("disconnect", () => {
+//     console.log("Rozłączono WebSocket");
+//   });
+
+//   socket.emit("streamers", emitStreamersUpdated());
+//   // Obsługa zdarzeń i logika WebSocket
+//   // ...
+// });
+
+// export async function emitStreamersUpdated() {
+//   try {
+//     const streamers = await getStreamers();
+//     io.emit("streamers", streamers);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
