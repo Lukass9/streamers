501ba6901d20df393aa8551152f74dfca04b7b7e
Lukass9 <l.pisarek@o2.pl>
2023-06-28 07:29:27 +0200

    added agagin socekt.io to api folder

--- api/index.js
+++ api/index.js
@@ -1,17 +1,33 @@
 import express from "express";
 import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 import useStreamers from "./routes/streamers.js";
+
 const app = express();
 const corsOptions = {
   origin: "http://localhost:5173",
   credentials: true,
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/streamers", useStreamers);
 
-app.listen(8800, () => {
-  console.log("Connected!");
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("A user connected!");
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected!");
+  });
+});
+
+const PORT = 8800;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
